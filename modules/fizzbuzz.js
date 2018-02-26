81233f02078a73a3d338c646df7419c8bb755646
grebe1991 <grebenita@gmail.com>
2018-02-27 01:14:52 +0200

    first day

--- modules/fizzbuzz.js
+++ modules/fizzbuzz.js
@@ -4,7 +4,7 @@
 
 fizzbuzz = () => {
     for (let i = 0; i <= 100; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
+        if (i % 15 === 0) {
             console.log('FizzBuzz');
             continue;
         }
